27651e279622d0292bd6068a331156fd200f3763
PascoPrzyborowski <unity01@gmx.net>
2022-08-25 15:54:58 +0200

    fatrex6

--- fstrex6.py
+++ fstrex6.py
@@ -6,10 +6,47 @@
 
 
 
-name = "Hello"
-firstname = "Gregor"
-print(f"{name} {firstname}")
+# name = "Hello"
+# firstname = "Gregor"
+# print(f"{name} {firstname}")
 
-name = "Hello Gregor"
-f"Hello, {name}!"
-print(name)
+# name = "Hello Gregor"
+# f"Hello, {name}!"
+# print(name)
+
+
+# given_string = """One morning, when Gregor Samsa woke from troubled dreams, 
+# he found himself transformed in his bed into a horrible vermin.
+# He lay on his armour-like back, and if he lifted his head a 
+# little he could see his brown belly, slightly domed and divided by 
+# arches into stiff sections. The bedding was hardly able to cover 
+# it and seemed ready to slide off any moment. His many legs, pitifully thin 
+# compared with the size of the rest of him, waved about helplessly as he looked."""
+
+# print(given_string)
+
+# given_string.index("H") #index 123
+
+# given_string[123] 
+# Footer
+
+given_string = "there was a really nice story about a man transformed to a insect named Gregor. Hey how are you"
+
+given_string.index("H")
+H = given_string[80]
+
+given_string.index("a")
+a = given_string[7]
+
+given_string.index("l")
+l = given_string[15]
+
+given_string.index("o")
+o = given_string[26]
+
+given_string.find("Gregor")
+Gregor = given_string[72:78]
+
+Hello_Gregor = f'"{H}{a}{l}{l}{o} {Gregor}"'
+
+print(Hello_Gregor)
